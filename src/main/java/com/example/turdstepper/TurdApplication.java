d8a749ef83bbc6bacda243991c58fd6241faee50
Andreas Witten Honoré <114078622+andy-the-ape@users.noreply.github.com>
2023-05-03 14:00:57 +0200

    changed cursor

--- src/main/java/com/example/turdstepper/TurdApplication.java
+++ src/main/java/com/example/turdstepper/TurdApplication.java
@@ -3,20 +3,27 @@ package com.example.turdstepper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class TurdApplication extends Application {
+    Image shoeCursor;
+
+
     @Override
     public void start(Stage stage) throws IOException {
-
         TurdController controller = new TurdController();
         FXMLLoader fxmlLoader = new FXMLLoader(TurdApplication.class.getResource("game-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),controller.gameBoard.getBoardHeight(), controller.gameBoard.getBoardWidth(), Color.LIGHTGRAY);
         fxmlLoader.setController(controller);
+        shoeCursor = new Image("src/main/resources/icons/shoe16x16.png");
+        scene.setCursor(new ImageCursor(shoeCursor));
 //        Group root = new Group();
 //        Scene scene = new Scene(root,gameBoard.getBoardHeight(), gameBoard.getBoardWidth(), Color.LIGHTGRAY);
         stage.setTitle("Turd Stepper");
